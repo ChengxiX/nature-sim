4c1026d9b31b3bc1a61ea01ef9ad048daf6407e1
ChengxiX <hanson.xcx@outlook.com>
2020-07-17 22:27:29 +0800

    Update sim.py

--- sim.py
+++ sim.py
@@ -4,7 +4,7 @@ from basic_genes import *
 if __name__ == '__main__':
     sun = suns(100)
     plants = []
-    plants.append(producer(sun, plants, 20, 10))
+    producer(sun, plants, 20, 10)
     for x in range(1000):
         sun.shine()
     print(sun.sunlight_queue)
