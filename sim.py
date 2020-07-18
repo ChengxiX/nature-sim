1b62a88656b0f3953abe30507f579019b6719c23
ChengxiX <hanson.xcx@outlook.com>
2020-07-18 22:27:59 +0800

    Update sim.py
    添加简单可视化

--- sim.py
+++ sim.py
@@ -1,5 +1,7 @@
 from basic_genes import *
 
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
     sun = suns(100)
     producers = []
@@ -9,10 +11,26 @@ if __name__ == '__main__':
     for i in range(10):
         alga(0.05, 0.8)
         alga(0.08, 1)
-    print(sun.sunlight_queue)
-    while True:
+    #print(sun.sunlight_queue)
+    sums_of_trees = []
+    sums_of_alags = []
+    for t in range(10):
         sun.shine()
-        print('sum of producer', len(producers))
-        print(sun.sunlight_queue)
-        if input():
-            break
+        sum_of_alags = 0
+        sum_of_trees = 0
+        for i in producers:
+            if isinstance(i, alga):
+                sum_of_alags += 1
+            elif isinstance(i, tree):
+                sum_of_trees += 1
+        sums_of_alags.append(sum_of_alags)
+        sums_of_trees.append(sum_of_trees)
+    #while True:
+        #sun.shine()
+        #print('sum of producer', len(producers))
+        #print(sun.sunlight_queue)
+        #if input():
+            #break
+    plt.plot(sums_of_trees, marker='.')
+    plt.plot(sums_of_alags, marker='.')
+    plt.show()
